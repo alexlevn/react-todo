760ca3f814b33ea3f03436dd8250cf2d10aadc31
thanhdungfb <thanhdungfb@gmail.com>
2018-04-16 15:34:47 +0700

    add status todolist

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
-var React = require('react')
+var React = require('react');
+var TodoList = require('TodoList');
+
 
 var TodoApp = React.createClass({
+    getInitialState: function(){
+        return{
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },{
+                    id: 2,
+                    text: 'Clear the yard'
+                },{
+                    id: 3,
+                    text: 'Fuct the code'
+                },{
+                    id: 4,
+                    text: 'Play a video'
+                }
+            ]
+        };
+    },
     render: function(){
+        // debugger;
+        var {todos} = this.state;
+
         return(
             <div>
-                TodoApp.jsx
+                <TodoList todos= {todos} />
             </div>
         )
     }
